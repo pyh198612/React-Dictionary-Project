5745128cce702f66e09bf69330d4d98aca29a32d
pyh198612 <76249346+pyh198612@users.noreply.github.com>
2021-09-19 19:45:57 +0200

    implemented search engine (Pending styling)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React from "react";
+import Dictionary from "./Dictionary";
 import './App.css';
 
-function App() {
+
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+      <div className="container">
+        <h1>Dictionary</h1>
+        <main>
+        <Dictionary />
+        </main>
+        <footer>This project was coded by Jennifer Hsieh, and it is open-sourced on GitHub & hosted on Netlify</footer>
+      </div>
     </div>
   );
 }
 
-export default App;
+
